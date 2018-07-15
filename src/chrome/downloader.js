2975f7465225ff21a9921c8e91cc3090be5b27ef
Hassan <haljadooa@gmail.com>
2018-07-15 01:21:24 -0700

    reverted back to using Blobs for now

--- src/chrome/downloader.js
+++ src/chrome/downloader.js
@@ -1,6 +1,7 @@
 // run the script after the page has fully loaded
 // to avoid potential errors in the execution
 window.addEventListener("load", function() {
+        const MIME_TYPE = "arraybuffer"
 
         let html = document.createElement("div"),
         content = "<div class=\"tw-inline-flex\">" 
@@ -9,6 +10,7 @@ window.addEventListener("load", function() {
 
         let player = document.getElementsByClassName("player-video"),
                 url = player[0].firstChild.getAttribute("src"),
+                bb = new Blob([url], {type: MIME_TYPE}),
                 sidebar = document.getElementsByClassName("clips-sidebar")[0].lastChild
 
         // add classes to the parent div 
@@ -22,5 +24,5 @@ window.addEventListener("load", function() {
         // select the download button
         // and assign the video url to the "href" attr
         let dlButton = document.getElementById("download-btn");
-        dlButton.setAttribute("href", url);
+        dlButton.setAttribute("href", window.URL.createObjectURL(bb));
 });
